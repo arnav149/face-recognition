add9fd7cd8059ccb6d81396a7df2c31bcac86c81
Arnav Kasturia <arnavkasturia@hotmail.com>
2020-06-08 00:30:55 -0700

    updating package.json

---
+++ build/precache-manifest.6eb75da5f4fa82380e569936d9d8f731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa9c02972cf3355ad50",
+    "url": "/face-recognition/static/css/main.7a080202.chunk.css"
+  },
+  {
+    "revision": "bfa9c02972cf3355ad50",
+    "url": "/face-recognition/static/js/main.bfa9c029.chunk.js"
+  },
+  {
+    "revision": "700414298392e89c579c",
+    "url": "/face-recognition/static/css/1.0aae30d8.chunk.css"
+  },
+  {
+    "revision": "700414298392e89c579c",
+    "url": "/face-recognition/static/js/1.70041429.chunk.js"
+  },
+  {
+    "revision": "00a7b039938598a9eb93",
+    "url": "/face-recognition/static/js/runtime~main.00a7b039.js"
+  },
+  {
+    "revision": "721fe372694a9bfea0576fa4e4ced0c6",
+    "url": "/face-recognition/index.html"
+  }
+];
